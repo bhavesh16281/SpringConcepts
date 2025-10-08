16046e7aba736aaa4d34ecdf548f62ee8ee20e57
Bhavesh <muthyalu.bhavesh@accenture.com>
2025-10-08 21:28:11 +0530

    sjdn

--- src/main/java/com/bhavesh16281/springAnnotations/ComponentAnnotation.java
+++ src/main/java/com/bhavesh16281/springAnnotations/ComponentAnnotation.java
@@ -1,5 +1,10 @@
 package com.bhavesh16281.springAnnotations;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan()
 public class ComponentAnnotation {
 
 }
